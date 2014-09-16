e62330f660c3e12902d16023f803a560d0501732
luisbaray <luisbaray@hotmail.com>
2014-09-15 21:49:46 -0600

    Departamentos Se agregaron
    Se agregaron departamentos que faltaban y se eliminaron los que no deben
    estar en el menú.

--- Controllers/DepartamentosController.cs
+++ Controllers/DepartamentosController.cs
@@ -16,10 +16,7 @@ namespace ITCH2_1.Controllers
             return View();
         }
 
-        public ActionResult SistemasComputacionales()
-        {
-            return View();
-        }
+       
 
         public ActionResult CienciasBasicas()
         {
@@ -27,6 +24,11 @@ namespace ITCH2_1.Controllers
         }
 
         public ActionResult IngenieriaIndustrial()
+        {
+            return View();
+        }
+
+        public ActionResult SistemasComputacionales()
         {
             return View();
         }
@@ -41,27 +43,18 @@ namespace ITCH2_1.Controllers
             return View();
         }
 
-        public ActionResult ServiciosEscolares()
-        {
-            return View();
-        }
-
         public ActionResult DivisionEstudiosProfesionales()
         {
             return View();
         }
 
-        public ActionResult GestionTecnologicaVinculacion()
+        public ActionResult DesarrolloAcademico()
         {
             return View();
         }
 
-        public ActionResult RecursosFinancieros()
-        {
-            return View();
-        }
 
-        public ActionResult CentroInformacion()
+        public ActionResult ServiciosEscolares()
         {
             return View();
         }
@@ -71,10 +64,46 @@ namespace ITCH2_1.Controllers
             return View();
         }
 
-        public ActionResult DesarrolloAcademico()
+        public ActionResult CentroInformacion()
         {
             return View();
         }
+
+        public ActionResult ComunicacionYDifusion()
+        {
+            return View();
+        }
+
+        public ActionResult GestionTecnologicaVinculacion()
+        {
+            return View();
+        }
+
+        public ActionResult PlaneacionProgramYPresup()
+        {
+            return View();
+        }
+
+        public ActionResult RecursosFinancieros()
+        {
+            return View();
+        }
+
+        public ActionResult RecursosHumanos()
+        {
+            return View();
+        }
+
+        public ActionResult RecursosMaterialesYServicios()
+        {
+            return View();
+        }
+
+        public ActionResult CentroDeComputo()
+        {
+            return View();
+        }
+               
 
 
     }
