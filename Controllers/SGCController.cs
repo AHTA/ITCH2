46803e0004986e9646be8908cf14bfae48091648
luisbaray <luisbaray@hotmail.com>
2014-09-17 19:36:54 -0600

    Se inició Sistema de Gestión de Calidad
    Se hizo la estructura, faltan los contenidos y ligas a los documentos.

---
+++ Controllers/SGCController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITCH2_1.Controllers
+{
+    public class SGCController : Controller
+    {
+        //
+        // GET: /SGC/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EquidadGenero()
+        {
+            return View();
+        }
+
+        public ActionResult Ambiental()
+        {
+            return View();
+        }
+
+        public ActionResult Calidad()
+        {
+            return View();
+        }
+    }
+}
