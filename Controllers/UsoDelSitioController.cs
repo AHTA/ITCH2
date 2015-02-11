c072829b07f4799998e8ab5cd1a6253dc7e589eb
Luis Baray <webmaster@itchihuahuaii.edu.mx>
2015-02-10 18:07:51 -0700

    Se cambió el menú "Mapa del Sitio" por "Uso del Sitio"
    Se trata de una página que explicará cómo usar el sitio, ya que hay
    muchas dudas entre alumnos y personal al respecto. La información se
    agregará día a día.

---
+++ Controllers/UsoDelSitioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITCH2_1.Controllers
+{
+    public class UsoDelSitioController : Controller
+    {
+        //
+        // GET: /UsoDelSitio/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
